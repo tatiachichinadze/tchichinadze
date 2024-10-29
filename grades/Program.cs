13d911fbccd9ca28f92e7b49c3aa36c8a7712b9d
Tatia Chichinadze <echichinadze@credo.ge>
2024-10-29 23:37:34 +0400

    Add project files.

---
+++ grades/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Runtime;
+
+int[,] grades = new int[3, 2];
+string[] studentNames = new string[3];
+
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.WriteLine($"Enter students name {i + 1};");
+    studentNames[i] = Console.ReadLine();
+
+}
+
+{for (int i = 0; i < grades.GetLength(0); i++)
+ for (int j = 0; j < grades.GetLength(1); j++)
+    {
+      Console.Write($"Enter students {studentNames[i]} {j + 1} score: ");
+      grades[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine("student scores:");
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.WriteLine($"{studentNames[i]}: {grades[i, 0]}, {grades[i, 1]}");
+}
+
+
